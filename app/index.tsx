6b5db7f0cf7eeab037909eb46f2a810167d74212
xwebcreative <xwebcreative82@gmail.com>
2025-07-04 15:53:02 +0100

    2nd commit, index ianx

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit ian app/index.tsx to edit this screen.</Text>
+      <Text>Edit ianx app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
